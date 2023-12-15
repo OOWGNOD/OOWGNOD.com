905b81508e71cd2da16affd8e551f3c74816901a
DONGWOO YU <86402288+OOWGNOD@users.noreply.github.com>
2023-12-15 17:02:12 +0900

    Update 2023-12-15-프로그래머스-멀리뛰기.m

--- _posts/2023-12-15-프로그래머스-멀리뛰기.m
+++ _posts/2023-12-15-프로그래머스-멀리뛰기.m
@@ -1,5 +1,3 @@
-[##_Image|kage@AU5Vi/btsBfgZ669w/tKDIyDUGq0L9os4uM7xbXK/img.png|CDM|1.3|{"originWidth":512,"originHeight":512,"style":"alignCenter","filename":"javascript_icon_153511.png"}_##]
-
 > 문제 설명  
 > 효진이는 멀리 뛰기를 연습하고 있습니다. 효진이는 한번에 1칸, 또는 2칸을 뛸 수 있습니다. 칸이 총 4개 있을 때, 효진이는  
 > (1칸, 1칸, 1칸, 1칸)  
